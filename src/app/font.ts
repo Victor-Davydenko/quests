34749db6a7ac5b4bca177f7ca4928d9aad861c8e
Victor Davydenko <acc.davydenko@gmail.com>
2024-04-20 13:35:18 +0300

    initial setup

---
+++ src/app/font.ts
@@ -0,0 +1,5 @@
+import { Raleway } from 'next/font/google';
+
+const raleway = Raleway({ subsets: ['cyrillic'], display: 'swap', weight: ['400', '500', '600', '700', '900'] });
+
+export default raleway;
