cb900fa797eeb8e51acc02ff4148d7f971c953bb
Victor Davydenko <acc.davydenko@gmail.com>
2024-04-25 16:21:18 +0300

    adjust quests component

--- src/components/QuestCard.tsx
+++ src/components/QuestCard.tsx
@@ -4,15 +4,8 @@ import Image from 'next/image';
 import Title from '@/components/common/Title';
 import Icons from '@/components/common/Icons';
 import { IQuest } from '@/interfaces/interfaces';
+import { questLevelDictionary } from '@/contstants/constants';
 
-interface IDictionary {
-  [key: string]: string
-}
-const QuestLevelDictionary: IDictionary = {
-  easy: 'легкий',
-  medium: 'середній',
-  hard: 'складний',
-};
 const QuestCard: FC<IQuest> = ({
   id, previewImg, title, level, peopleCount,
 }): ReactElement => {
@@ -30,7 +23,7 @@ const QuestCard: FC<IQuest> = ({
         </span>
         <span className='flex gap-x-2 items-center'>
           <Icons.puzzle />
-          {QuestLevelDictionary[level]}
+          {questLevelDictionary[level]}
         </span>
       </div>
     </Link>
