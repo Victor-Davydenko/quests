a15d76cdc1a9e0125b7b62c809063df7803253b3
Victor Davydenko <acc.davydenko@gmail.com>
2024-05-01 11:52:17 +0300

    refactor markup in Promo.tsx

--- src/components/Promo.tsx
+++ src/components/Promo.tsx
@@ -15,26 +15,26 @@ const Promo: FC<IPromoProps> = ({
 }) => {
   const [from, to] = peopleCount;
   return (
-    <div className='w-full max-w-[600px]'>
+    <article className='w-full max-w-[600px]'>
       <Title level={1} className='text-sm text-orange -ml-[30px]'>{typeQestDictionary[type]}</Title>
       <Title level={2} className='text-white text-[92px] font-extrabold leading-none -ml-[30px] mb-8'>{title}</Title>
-      <div className='flex gap-x-12 mb-5'>
-        <span className='flex gap-x-1 items-center'>
+      <ul className='flex gap-x-12 mb-5'>
+        <li className='flex gap-x-1 items-center'>
           <Icons.clock />
           <span>{`${duration} хв.`}</span>
-        </span>
-        <span className='flex gap-x-1 items-center'>
+        </li>
+        <li className='flex gap-x-1 items-center'>
           <Icons.person />
           <span>{`${from} - ${to} люд.`}</span>
-        </span>
-        <span className='flex gap-x-1 items-center'>
+        </li>
+        <li className='flex gap-x-1 items-center'>
           <Icons.puzzle />
           <span>{questLevelDictionary[level]}</span>
-        </span>
-      </div>
+        </li>
+      </ul>
       <p className='mb-10'>{description}</p>
       <ButtonLink href='#'>Забронювати</ButtonLink>
-    </div>
+    </article>
   );
 };
 
