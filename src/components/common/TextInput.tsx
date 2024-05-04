22a0e1856f59b74a54e0771630c3e078cfcc6ecb
Victor Davydenko <acc.davydenko@gmail.com>
2024-05-04 13:34:41 +0300

    add TextInput.tsx

---
+++ src/components/common/TextInput.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import React, { FC } from 'react';
+import { FieldError, UseFormRegister } from 'react-hook-form';
+import { IForm, ValidFieldNames } from '@/interfaces/interfaces';
+
+interface ITextInput {
+  id: ValidFieldNames,
+  label: string,
+  placeholder: string
+  error: FieldError | undefined
+  register: UseFormRegister<IForm>
+  valueAsNumber?: boolean
+}
+const TextInput: FC<ITextInput> = ({
+  id, label, register, error, valueAsNumber = undefined, ...attrs
+}) => {
+  return (
+    <div
+      className='relative text-text_white font-medium w-full h-[55px] border border-text_white rounded bg-transparent mb-16'
+    >
+      <label htmlFor={id} className='text-[15px] absolute -top-8'>{label}</label>
+      <input
+        {...register(id, valueAsNumber ? { valueAsNumber: true } : {})}
+        id={id}
+        {...attrs}
+        className='text-sm w-full h-full bg-transparent px-8 appearance-none outline outline-0 focus:outline-0'
+      />
+      {error && <div className='text-orange text-left'>{error.message}</div>}
+    </div>
+  );
+};
+
+export default TextInput;
