e4e9b9f57d66d0c22a66a4337853e8df6cef06f2
Victor Davydenko <acc.davydenko@gmail.com>
2024-05-04 13:35:23 +0300

    add function to createOrder

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,4 +1,4 @@
-import { IQuest } from '@/interfaces/interfaces';
+import { IOrder, IQuest } from '@/interfaces/interfaces';
 
 export const getQuests = async ():Promise<IQuest[]> => {
   const response = await fetch(`${process.env.API_URL}/quests`, { cache: 'no-cache' });
@@ -13,4 +13,20 @@ export const getSingleQuest = async (id: string):Promise<IQuest> => {
   const response = await fetch(`${process.env.API_URL}/quests/${id}`);
   const quest = await response.json();
   return quest;
+};
+
+export const createOrder = async (orderData: IOrder) => {
+  try {
+    const response = await fetch('http://localhost:3001/orders', {
+      method: 'POST',
+      body: JSON.stringify(orderData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await response.json();
+    return data;
+  } catch (e: any) {
+    throw new Error(e.message);
+  }
 };
