886457a057712117a41c37c173620d59432d55a5
Victor Davydenko <acc.davydenko@gmail.com>
2024-05-14 16:23:11 +0300

    fix image path

--- src/components/QuestCard.tsx
+++ src/components/QuestCard.tsx
@@ -13,7 +13,7 @@ const QuestCard: FC<IQuest> = ({
   return (
     <Link href={`detailed-quest/${id}`} className='relative max-w-[345px]'>
       <div className='absolute rounded bg-card_bg shadow-card_shadow w-full h-full' />
-      <Image src={`http://localhost:3001/${previewImg}`} alt='card' width={345} height={230} className='rounded' />
+      <Image src={`/${previewImg}`} alt='card' width={345} height={230} className='rounded' />
       <Title level={2} className='absolute left-5 bottom-16 font-bold text-white text-2xl'>{title}</Title>
       <div className='absolute flex items-center gap-x-4 left-5 bottom-8 font-medium text-text_white text-sm'>
         <span className='flex items-center'>
