c68a919c42087f5d4b1d7e093ea2bda1f7008768
Victor Davydenko <acc.davydenko@gmail.com>
2024-05-21 14:01:57 +0300

    add signin signup form validation

--- src/formValidation/formValidation.ts
+++ src/formValidation/formValidation.ts
@@ -1,6 +1,6 @@
 import { z, ZodType } from 'zod';
 import validator from 'validator';
-import { IForm } from '@/interfaces/interfaces';
+import { IForm, ISignInForm, ISignUpForm } from '@/interfaces/interfaces';
 
 const formValidationSchema:ZodType<IForm> = z.object({
   name: z.string().min(1, 'Це обов\'язкове поле'),
@@ -9,6 +9,17 @@ const formValidationSchema:ZodType<IForm> = z.object({
     invalid_type_error: 'Введіть кількість гравців',
   }).min(1, 'Це обов\'язкове поле').max(10, 'Максимальна кількість гравців - 10'),
   privateDataAgreement: z.boolean().refine((value) => value, 'Це обов\'язкове поле'),
+});
+
+export const signUpValidationSchema:ZodType<ISignUpForm> = z.object({
+  email: z.string().email('Введіть email'),
+  password: z.string().min(1, 'Це обов\'язкове поле'),
+  confirmPassword: z.string().min(1, 'Це обов\'язкове поле'),
+}).refine((values) => values.password === values.confirmPassword, { message: 'Паролі повинні співпадати', path: ['confirmPassword'] });
+
+export const signInValidationSchema:ZodType<ISignInForm> = z.object({
+  email: z.string().email('Введіть email'),
+  password: z.string().min(1, 'Це обов\'язкове поле'),
 });
 
 export default formValidationSchema;
