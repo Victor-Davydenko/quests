3c339b888d98e39b843c511a403d4e3fca3b3c24
Victor Davydenko <acc.davydenko@gmail.com>
2024-05-22 13:31:22 +0300

    add button to login or logout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,14 +3,19 @@ import Link from 'next/link';
 import Navigation from '@/components/Navigation';
 import Logo from '@/components/common/Logo';
 import { routes } from '@/contstants/constants';
+import { checkSession } from '@/http';
+import LogoutForm from '@/components/LogoutForm';
 
-const Header: FC = (): ReactElement => {
+const Header: FC = async (): Promise<ReactElement> => {
+  const session = await checkSession();
+  const logoutBtn = session ? <LogoutForm /> : <Link href='/signin'>Увійти</Link>;
   return (
     <header className='absolute w-full top-0 left-0 font-semibold text-text_white text-sm uppercase pt-6'>
       <div className='container mx-auto max-w-[1440px] flex justify-between items-center px-8'>
         <Logo />
         <Navigation routes={routes} />
         <Link href='tel:0501112222' className='leading-none'>050 111 22 22</Link>
+        {logoutBtn}
       </div>
     </header>
   );
