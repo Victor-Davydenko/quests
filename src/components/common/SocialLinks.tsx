96d494e17dcf6abef56fdbbb6168173bb129e4c8
Victor Davydenko <acc.davydenko@gmail.com>
2024-05-24 17:50:49 +0300

    add aria-label to links

--- src/components/common/SocialLinks.tsx
+++ src/components/common/SocialLinks.tsx
@@ -5,13 +5,13 @@ import Icons from '@/components/common/Icons';
 const SocialLinks = (): ReactElement => {
   return (
     <>
-      <Link href='https://twitter.com'>
+      <Link href='https://twitter.com' aria-label='twitter.com'>
         <Icons.twitter className='w-[25px] h-auto' />
       </Link>
-      <Link href='https://instagram.com'>
+      <Link href='https://instagram.com' aria-label='instagram.com'>
         <Icons.instagram className='w-[25px] h-auto' />
       </Link>
-      <Link href='https://youtube.com'>
+      <Link href='https://youtube.com' aria-label='youtube.com'>
         <Icons.youtube className='w-[25px] h-auto' />
       </Link>
     </>
