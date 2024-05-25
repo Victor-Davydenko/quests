2d1a9c2cbb4facbc371e85c542869ba66d3611a4
Victor Davydenko <acc.davydenko@gmail.com>
2024-05-25 23:21:28 +0300

    add middleware to protect routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,20 @@
 import { i18nRouter } from 'next-i18n-router';
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+import { SESSION_COOKIE } from '@/contstants/constants';
+import { cookies } from 'next/headers';
 import i18nConfig from '../i18nConfig';
 
 export function middleware(request: NextRequest) {
+  const sessionCookie = cookies().get(SESSION_COOKIE)?.value;
+  const path = request.nextUrl.pathname;
+  if (!sessionCookie && path.includes('/bookQuest')) {
+    const absoluteURL = new URL('/signin', request.nextUrl.origin);
+    return NextResponse.redirect(absoluteURL.toString());
+  }
+  if (sessionCookie && (path.includes('signin') || path.includes('signup'))) {
+    const absoluteURL = new URL('/', request.nextUrl.origin);
+    return NextResponse.redirect(absoluteURL.toString());
+  }
   return i18nRouter(request, i18nConfig);
 }
 
