c4fe94554608990d8dc766aecdad2f7f58c16b32
Victor Davydenko <acc.davydenko@gmail.com>
2024-05-28 18:31:49 +0300

    add mobile style to logout btn

--- src/components/LogoutForm.tsx
+++ src/components/LogoutForm.tsx
@@ -5,7 +5,7 @@ import { logout } from '@/http';
 const LogoutForm: FC<{ text: string }> = ({ text }) => {
   return (
     <form action={logout}>
-      <Button type='submit' className='text-m bg-orange uppercase text-white font-medium text-center tracking-[2px] px-4 py-2 min-w-[150px] rounded-full inline-block'>{text}</Button>
+      <Button type='submit' className='text-m bg-orange uppercase text-white font-medium text-center tracking-[2px] px-4 py-2 sm:min-w-[150px] rounded-full inline-block'>{text}</Button>
     </form>
   );
 };
