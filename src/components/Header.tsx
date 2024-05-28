9bc18a508fc6d6ef3245a08852fddf4d5ea3daf4
Victor Davydenko <acc.davydenko@gmail.com>
2024-05-28 18:35:02 +0300

    add burger menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,33 @@
-import { FC, ReactElement } from 'react';
-import Link from 'next/link';
+'use client';
+
+import {
+  FC, ReactElement, ReactNode, useState,
+} from 'react';
+import dynamic from 'next/dynamic';
 import Navigation from '@/components/Navigation';
 import Logo from '@/components/common/Logo';
-import { routes, SESSION_COOKIE } from '@/contstants/constants';
-import LogoutForm from '@/components/LogoutForm';
-import initTranslations from '@/app/i18n';
-import dynamic from 'next/dynamic';
-import { cookies } from 'next/headers';
+import PhoneLink from '@/components/common/PhoneLink';
+import { routes } from '@/contstants/constants';
+import clsx from 'clsx';
+import BurgerMenuOpener from '@/components/common/BurgerMenuOpener';
 
 const LangSwitcher = dynamic(() => import('@/components/LangSwitcher'), { ssr: false });
 
-const Header: FC<{ locale: string }> = async ({ locale }): Promise<ReactElement> => {
-  const { t } = await initTranslations(locale, ['auth']);
-  const logoutBtnText = t('logout_btn');
-  const logoutBtn = cookies().get(SESSION_COOKIE)?.value ? <LogoutForm text={logoutBtnText} /> : <Link href='/signin'>{t('login_btn')}</Link>;
+const Header: FC<{ auth: ReactNode }> = ({ auth }): ReactElement => {
+  const [isMobileOpen, setIsMobileOpen] = useState(false);
+  const openMobileMenuHandler = () => {
+    setIsMobileOpen((prevState) => !prevState);
+  };
+  const styles = isMobileOpen ? { display: 'block' } : { display: '' };
   return (
-    <header className='absolute w-full top-0 left-0 font-semibold text-text_white text-sm uppercase pt-6'>
-      <div className='container mx-auto max-w-[1440px] flex justify-between items-center px-8'>
+    <header className='absolute w-full top-0 left-0 font-semibold text-text_white text-sm uppercase pt-6 pb-6 bg-page_bg'>
+      <div className={clsx({ flex: !isMobileOpen, block: isMobileOpen }, 'container mx-auto max-w-[1440px] gap-x-3 md:gap-x-unset sm:justify-between items-center px-8')}>
         <Logo />
-        <Navigation routes={routes} />
-        <Link href='tel:0501112222' className='leading-none'>050 111 22 22</Link>
-        {logoutBtn}
-        <LangSwitcher />
+        <Navigation styles={styles} routes={routes} />
+        <PhoneLink styles={styles} />
+        {!isMobileOpen ? auth : null}
+        {!isMobileOpen ? <LangSwitcher /> : null}
+        <BurgerMenuOpener handler={openMobileMenuHandler} />
       </div>
     </header>
   );
