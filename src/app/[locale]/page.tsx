3251610c2cc678dcd39378fdab57f1dd5c6404d2
Victor Davydenko <acc.davydenko@gmail.com>
2024-05-28 18:30:35 +0300

    add mobile style to homepage paragraph

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -19,7 +19,7 @@ export default async function Home({
     <section className='bg-page_bg h-full min-h-screen pt-32 w-full'>
       <div className='container mx-auto max-w-[1200px] px-5'>
         <Title level={1} className='text-orange text-sm font-medium leading-5'>{t('homepage_title')}</Title>
-        <p className='text-white font-extrabold text-[64px] mb-12'>{t('homepage_question')}</p>
+        <p className='text-white font-extrabold text-3xl sm:text-5xl md:text-[64px] mb-12'>{t('homepage_question')}</p>
         <FilterBar filter={searchParams.filter} locale={locale} />
         <div className='min-h-screen'>
           <Suspense fallback={<Loading />}>
